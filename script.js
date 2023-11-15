c58373d136f749b0425fc3f9ac4ceadec3db79d9
Swantje Moehle <28712729+SwantjeM@users.noreply.github.com>
2023-11-15 11:34:12 -0800

    changed storage to session storage

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const textForm = document.getElementById("textForm");
 const textInput = document.getElementById("textInput");
 const saveButton = document.getElementById("saveButton");
 
+var countdownTime = 15; // 3 minutes * 60 seconds
+var timerElement = document.getElementById('timer');
+var startButton = document.getElementById('startButton');
+var modal = document.getElementById('myModal');
+var confirmButton = document.getElementById('confirmButton');
+var cancelButton = document.getElementById('cancelButton');
+
+
 var currentDate = new Date();
 
 let entries = [];
-
-// Check if the array already exists in local storage
-if (localStorage.getItem("entries")) {
-    // If it exists, load the array from local storage
-    entries = JSON.parse(localStorage.getItem("entries"));
-}
 
 // Add an event listener to the Save button
 saveButton.addEventListener("click", function () {
@@ -20,37 +22,20 @@ saveButton.addEventListener("click", function () {
 
     // Check if the input is not empty
     if (inputText.trim() !== "") {
-        // Store the text in local storage with a specific key
-        //        localStorage.setItem("userText", inputText);
-
-        // Add a new entry to the array
-        //   const newEntry = "New entry text"; // Replace with your new entry
         newEntry = {
             "name": inputText,
-            "timestamp": currentDate,  // Use a valid timestamp in ISO 8601 format
+            "timestamp": currentDate,
         }
 
         entries.push(newEntry);
 
-        // Save the updated array to local storage
-        localStorage.setItem("entries", JSON.stringify(entries));
         // Optionally, provide user feedback
-        //  alert("Text saved to local storage!");
+        // alert("Text saved temporarily!");
 
         // Clear the input field
         textInput.value = "";
     }
 });
-
-// Check if the text is already stored in local storage
-if (localStorage.getItem("entries")) {
-    // Retrieve the stored text
-    var myList = document.getElementById("listpresents");
-
-    const storedText = localStorage.getItem("entries");
-}
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
     var myList = document.getElementById("listpresents");
@@ -58,31 +43,58 @@ document.addEventListener("DOMContentLoaded", function () {
     // Get the current date
     var currentDate = new Date();
 
-    // Loop through the JSON data and create list items for entries from yesterday
+    // Loop through 
     entries.forEach(function (item) {
         var entryTimestamp = new Date(item.timestamp);
 
-        // Check if the entry's timestamp is from yesterday
+        // Check if the entry's timestamp is from today
         if (entryTimestamp.getDate() === currentDate.getDate()) {
             var li = document.createElement("li");
             li.textContent = item.name //+ ': ' + item.timestamp;
             myList.appendChild(li);
         }
-
-        // Loop through the JSON data and create list items
-        //    entries.forEach(function (item) {
-        //      var li = document.createElement("li");
-        //    li.textContent = item.name + ': ' + item.timestamp;
-        //  myList.appendChild(li);
     });
 });
 
-var countdownTime = 15; // 3 minutes * 60 seconds
-var timerElement = document.getElementById('timer');
-var startButton = document.getElementById('startButton');
-var modal = document.getElementById('myModal');
-var confirmButton = document.getElementById('confirmButton');
-var cancelButton = document.getElementById('cancelButton');
+// Display the entries when the timer runs out and user confirms
+function displayEntries() {
+    var myList = document.getElementById("listpresents");
+
+    entries.forEach(function (item) {
+        var li = document.createElement("li");
+        li.textContent = item.name;
+        myList.appendChild(li);
+    });
+}
+
+// When the timer runs out...
+function timerRunOut() {
+    timerElement.textContent = '00:00';
+   // alert('Time is up!');
+
+    // Display a confirmation dialog
+    var confirmation = window.confirm('Do you want to see your entries?');
+
+    if (confirmation) {
+        displayEntries();
+    }
+}
+
+// If the user clicks "OK" in the modal, start the timer
+confirmButton.onclick = function () {
+    startCountdown();
+};
+
+// If the user clicks "Cancel" in the modal, you can handle this case as needed
+cancelButton.onclick = function () {
+    alert('Timer not started');
+    modal.style.display = 'none';
+};
+
+// Show the modal when the page loads
+modal.style.display = 'block';
+
+
 
 function updateTimer() {
     var minutes = Math.floor(countdownTime / 60);
@@ -109,7 +121,7 @@ function countdown() {
         setTimeout(countdown, 1000); // Update every second
     } else {
         timerElement.textContent = '00:00';
-        alert('Time is up!');
+        timerRunOut();
     }
 }
 
